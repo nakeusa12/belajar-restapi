d9e4c22995b84086a045b59b89ffe769341a376a
Saiful Nakeusa <snakeusa8@gmail.com>
2020-08-11 11:25:25 +0700

    berhasil menampilkan data berdasarkan id

--- routes.js
+++ routes.js
@@ -3,9 +3,10 @@
 module.exports = function(app){
     var jsonku = require('./controller');
 
-    app.route('/')
-        .get(jsonku.index);
+    app.route('/').get(jsonku.index);
 
-    app.route('/tampil')
-        .get(jsonku.tampildata)
+    app.route('/tampil').get(jsonku.tampildata);
+
+    app.route('/tampil/:id').get(jsonku.tampilberdasarkanId);
 }
+
